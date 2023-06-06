a592b4bf0f645ab9d8270fb12efd4dc51cd2f5ee
Tomislav996 <trpeskitomislav@outlook.com>
2023-06-06 18:32:48 +0200

    .

--- React_homework/movie_library_app/src/Component_MovieList/MovieList.js
+++ React_homework/movie_library_app/src/Component_MovieList/MovieList.js
@@ -6,8 +6,13 @@ const MovieList = () => {
         {id: 1, name: "Drive", dateofproduction: "2011"},
         {id: 2, name: "The Dark Knight", dateofproduction: "2008"},
         {id: 3, name: "Gladiator", dateofproduction: "2000"},
-        {id: 4, name: "LOTR return of the king", dateofproduction: "2003"},
-        {id: 5, name: "The Bourne Identity", dateofproduction: "2002"}
+        {id: 4, name: "LOTR Return Of The King", dateofproduction: "2003"},
+        {id: 5, name: "The Bourne Identity", dateofproduction: "2002"},
+        {id: 6, name: "Taken", dateofproduction: "2008"},
+        {id: 7, name: "2001 A Space Odyssey", dateofproduction: "1968"},
+        {id: 8, name: "The Shining", dateofproduction: "1980"},
+        {id: 9, name: "The Machinist", dateofproduction: "2004"},
+        {id: 10, name: "LOTR The Two Towers", dateofproduction: "2002"}
     ]
 
     const message = `Hello, this is a simple React component that shows a list of movies`;
@@ -27,6 +32,7 @@ const MovieList = () => {
                     movies.map( movie => {
                         return(
                             <li key={movie.id}>Name: {movie.name}, Date of production: {movie.dateofproduction}</li>
+                            
                         )
                     })
                 }
