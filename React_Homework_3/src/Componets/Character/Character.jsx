0600ef99733e43709363e33c984862e0f3189dfd
Tomislav996 <trpeskitomislav@outlook.com>
2023-06-14 16:58:23 +0200

    update

--- React_Homework_3/src/Componets/Character/Character.jsx
+++ React_Homework_3/src/Componets/Character/Character.jsx
@@ -6,7 +6,7 @@ const Character = ({ character }) => {
   
   return (
     <div className='characters-container'>
-      <h3 className='name'>{character.name}</h3>
+      <h2 className='name'>{character.name}</h2>
       <p>Height: {character.height}</p>
       <p>Gender: {character.gender}</p>
       <p>Birth Year: {character.birth_year}</p>
